7f1f62e5a0dea62ea4a0ac2f5fee33edd917f869
MATTIAS SVEDKLINT <m.svedklint@telia.com>
2012-05-23 14:53:33 +0200

    En uppdatering

---
+++ site/src/CCMycontroller/om.tpl.php
@@ -0,0 +1,7 @@
+<h3>Om mig</h3>
+<img id=about src=<?=$img?> width="100" height="68" alt="Mig" border="0">
+<p>Jag som har gjort denna sida heter Mattias Svedklint och studerar på Blekinge teknika högskola.
+Denna sida är gjord för ett avslutnings projekt i en kurs som heter Webbapplikationer, PHP & MVC.
+<br><br>
+Utifrån mitt intresse för motorcyklar och främst Suzuki gsxr så blev det en sida om det.
+
